fe6ba7359af6bedbef6b9b41c897a09637080f45
Tania Zhuchenko <taniyhamail@gmail.com>
2025-01-07 18:10:23 +0200

    feat: added first route

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,8 @@ import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent).catch((err) => console.error(err));
+import { appConfig } from './app/app.config';
+
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);
