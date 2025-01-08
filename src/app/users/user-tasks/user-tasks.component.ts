2585a1b4dbed6016d78205d329d5a56855f87a80
Tania Zhuchenko <taniyhamail@gmail.com>
2025-01-08 13:07:19 +0200

    feat: added route links and relative liks

--- src/app/users/user-tasks/user-tasks.component.ts
+++ src/app/users/user-tasks/user-tasks.component.ts
@@ -6,13 +6,13 @@ import {
   input,
   OnInit,
 } from '@angular/core';
-import { ActivatedRoute, RouterOutlet } from '@angular/router';
+import { ActivatedRoute, RouterLink, RouterOutlet } from '@angular/router';
 import { UsersService } from '../users.service';
 
 @Component({
   selector: 'app-user-tasks',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './user-tasks.component.html',
   styleUrl: './user-tasks.component.css',
 })
