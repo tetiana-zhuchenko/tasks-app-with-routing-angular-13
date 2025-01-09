3e0b7b6ee354736e4cf518a4f4f0e7a687da6db9
Tania Zhuchenko <taniyhamail@gmail.com>
2025-01-09 11:50:50 +0200

    feat: refactored routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
-import { TasksComponent } from './tasks/tasks.component';
+
+import { routes as userRoutes } from './users/users.routes';
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
 import { UserTasksComponent } from './users/user-tasks/user-tasks.component';
-import { NewTaskComponent } from './tasks/new-task/new-task.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
@@ -15,24 +15,10 @@ export const routes: Routes = [
   {
     path: 'users/:userId', // <your-domain>/users/<uid>
     component: UserTasksComponent,
-    children: [
-      {
-        path: '',
-        redirectTo: 'tasks',
-        pathMatch: 'prefix',
-      },
-      {
-        path: 'tasks', // <your-domain>/users/<uid>/tasks
-        component: TasksComponent,
-      },
-      {
-        path: 'tasks/new', // <your-domain>/users/<uid>/tasks/new
-        component: NewTaskComponent,
-      },
-      {
-        path: '**',
-        component: NotFoundComponent,
-      },
-    ],
+    children: userRoutes,
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
   },
 ];
